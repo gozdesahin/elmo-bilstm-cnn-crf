69827cdb0b79643e4bf45535359fbf15f6b2b1a6
gozdesahin <goezde.guel@gmail.com>
2018-08-30 18:16:44 +0200

    eval pos scores

--- EvalModel_CoNLL_Format.py
+++ EvalModel_CoNLL_Format.py
@@ -36,5 +36,8 @@ addEmbeddings(dataMatrix, embLookup.sentenceLookup)
 # :: Tag the input ::
 tags = lstmModel.tagSentences(dataMatrix)
 
-test_pre, test_rec, test_f1 = lstmModel.computeF1('conll2000_data/perturbed/03',dataMatrix)
-print("Test-Data: Prec: %.3f, Rec: %.3f, F1: %.4f" % (test_pre, test_rec, test_f1))
+#test_pre, test_rec, test_f1 = lstmModel.computeF1('conll2000_data/clean',dataMatrix)
+#print("Test-Data: Prec: %.3f, Rec: %.3f, F1: %.4f" % (test_pre, test_rec, test_f1))
+
+test_acc = lstmModel.computeAcc('conll2000_data/clean',dataMatrix)
+print("Test-Data: Accuracy: %.3f" % (test_acc))
