92a076f24352829d27267a91e1d4f18fb91ae4ad
gozdesahin <goezde.guel@gmail.com>
2018-08-31 16:49:26 +0200

    no predict

--- EvalModel_CoNLL_Format.py
+++ EvalModel_CoNLL_Format.py
@@ -37,7 +37,7 @@ addEmbeddings(dataMatrix, embLookup.sentenceLookup)
 #tags = lstmModel.tagSentences(dataMatrix)
 
 # Evaluation of Chunking
-test_pre, test_rec, test_f1 = lstmModel.computeF1('conll2000_data/clean',dataMatrix)
+test_pre, test_rec, test_f1 = lstmModel.computeF1('conll2000_data/perturbed/03',dataMatrix)
 print("Test-Data: Prec: %.3f, Rec: %.3f, F1: %.4f" % (test_pre, test_rec, test_f1))
 
 # Evaluation of POS tagging
