e90205a3d7a4bd9831c20c020d46379c869146e1
gozdesahin <goezde.guel@gmail.com>
2018-09-17 17:08:53 +0200

    script friendly code

--- EvalModel_CoNLL_Format.py
+++ EvalModel_CoNLL_Format.py
@@ -8,39 +8,60 @@ import sys
 from util.preprocessing import addCharInformation, createMatrices, addCasingInformation, addEmbeddings, readCoNLL
 from neuralnets.ELMoBiLSTM import ELMoBiLSTM
 from neuralnets.ELMoWordEmbeddings import ELMoWordEmbeddings
+import argparse
+from util.postprocessing import get_last_model_path
 
-if len(sys.argv) < 3:
-    print("Usage: python EvalModel_CoNLL_Format.py modelPath inputPathToConllFile")
-    exit()
 
-modelPath = sys.argv[1]
-inputPath = sys.argv[2]
-inputColumns = {0: "tokens", 1:'POS', 2:'chunk_BIO'}
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-datasetName', type=str, default='conll2000_data/clean', help='necessary to find the model path')
+    parser.add_argument('-testFile', type=str, default='data/conll2000_data/clean/test.txt', help='conll file to test')
+    parser.add_argument('-testSetting', type=str, default='CT1.txt', help='conll file to test')
+    parser.add_argument('-cuda_device', type=int, default=0, help='cpu:-1, others: gpu')
+    parser.add_argument('-task', type=str, default='pos', help='pos|chunking')
+    args = parser.parse_args()
+    evaluate(args)
 
-# :: Load the model ::
-lstmModel = ELMoBiLSTM.loadModel(modelPath)
 
-# :: Prepare the input ::
-sentences = readCoNLL(inputPath, inputColumns)
-addCharInformation(sentences)
-addCasingInformation(sentences)
+def evaluate(args):
+    fpath = 'models/'+args.datasetName+'_1.h5'
+    save_dir, model_init = os.path.split(fpath)
 
-# :: Map casing and character information to integer indices ::
-dataMatrix = createMatrices(sentences, lstmModel.mappings, True)
+    modelPath, _ = get_last_model_path(save_dir, model_init)
+    inputPath = args.testFile
+    inputColumns = {0: "tokens", 1:'POS', 2:'chunk_BIO'}
 
-# :: Perform the word embedding / ELMo embedding lookup ::
-embLookup = lstmModel.embeddingsLookup
-embLookup.elmo_cuda_device = 0         #Cuda device for pytorch - elmo embedding, -1 for CPU
-addEmbeddings(dataMatrix, embLookup.sentenceLookup)
+    resfpath = 'results/'+args.task+'/'+args.testSetting
+    resfile = open(resfpath, 'w')
 
-# :: Tag the input ::
-#tags = lstmModel.tagSentences(dataMatrix)
+    # :: Load the model ::
+    lstmModel = ELMoBiLSTM.loadModel(modelPath)
 
-# Evaluation of Chunking
-test_pre, test_rec, test_f1 = lstmModel.computeF1('conll2000_data/perturbed/03',dataMatrix)
-print("Test-Data: Prec: %.3f, Rec: %.3f, F1: %.4f" % (test_pre, test_rec, test_f1))
+    # :: Prepare the input ::
+    sentences = readCoNLL(inputPath, inputColumns)
+    addCharInformation(sentences)
+    addCasingInformation(sentences)
 
-# Evaluation of POS tagging
-#test_acc = lstmModel.computeAcc('conll2000_data/perturbed/03',dataMatrix)
-#print("Test-Data: Accuracy: %.3f" % (test_acc))
+    # :: Map casing and character information to integer indices ::
+    dataMatrix = createMatrices(sentences, lstmModel.mappings, True)
 
+    # :: Perform the word embedding / ELMo embedding lookup ::
+    embLookup = lstmModel.embeddingsLookup
+    embLookup.elmo_cuda_device = 0                          #Cuda device for pytorch - elmo embedding, -1 for CPU
+    addEmbeddings(dataMatrix, embLookup.sentenceLookup)
+
+    if(args.task=="pos"):
+        # Evaluation of POS tagging
+        test_acc = lstmModel.computeAcc(args.datasetName, dataMatrix)
+        print("Test-Data: Accuracy: %.4f" % (test_acc))
+        resfile.write("Test-Data: Accuracy: %.4f" % (test_acc))
+    elif(args.task=="chunking"):
+        # Evaluation of Chunking
+        test_pre, test_rec, test_f1 = lstmModel.computeF1(args.datasetName, dataMatrix)
+        print("Test-Data: Prec: %.3f, Rec: %.3f, F1: %.4f" % (test_pre, test_rec, test_f1))
+        resfile.write("Test-Data: Prec: %.3f, Rec: %.3f, F1: %.4f" % (test_pre, test_rec, test_f1))
+
+    resfile.close()
+
+if __name__ == '__main__':
+    main()
---
+++ run_POS_Velmo_5e_CT.sh
@@ -0,0 +1,102 @@
+#!/bin/bash
+echo 'Clean Training Performed'
+DATASET='conll2000_data/clean'
+# (0) Remove previous caches and embeddings
+echo "Remove cached embeddings"
+rm ./embeddings/velmo_cache_conll2000_data_clean.pkl
+
+echo "Create new cached embeddings"
+# (1) Create cache for VELMO embeddings for clean data
+python Create_ELMo_Cache.py -datasetName $DATASET \
+-tokenColumnId 0 \
+-cuda_device 0 \
+-elmo_options 'pretrained/velmo_options.json' \
+-elmo_weights 'pretrained/velmo_weights.hdf5' \
+-pkl_path 'embeddings/velmo_cache_conll2000_data_clean.pkl'
+
+mkdir results
+mkdir results/pos
+
+# Do it 5 times
+for RUN in {1..5}
+do
+	echo "Train a POS tagger"
+	# (2) Train and test a POS tagger
+	python Train_POS.py -datasetName $DATASET \
+	-tokenColumnId 0 \
+	-cuda_device 0 \
+	-elmo_options 'pretrained/velmo_options.json' \
+	-elmo_weights 'pretrained/velmo_weights.hdf5' \
+	-pkl_path 'embeddings/velmo_cache_conll2000_data_clean.pkl'
+
+	echo "Test on clean data"
+	python EvalModel_CoNLL_Format.py -datasetName $DATASET \
+	-testFile 'data/conll2000_data/clean/test.txt' \
+	-testSetting 'CTCT_'$RUN'.txt' \
+	-cuda_device 0 \
+	-task 'pos'
+
+	echo "Test on p=0,1"
+	python EvalModel_CoNLL_Format.py -datasetName $DATASET \
+	-testFile 'data/conll2000_data/perturbed/01/test.txt' \
+	-testSetting 'CTP01_'$RUN'.txt' \
+	-cuda_device 0 \
+	-task 'pos'
+
+	echo "Test on p=0,2"
+	python EvalModel_CoNLL_Format.py -datasetName $DATASET \
+	-testFile 'data/conll2000_data/perturbed/02/test.txt' \
+	-testSetting 'CTP02_'$RUN'.txt' \
+	-cuda_device 0 \
+	-task 'pos'
+
+	echo "Test on p=0,3"
+	python EvalModel_CoNLL_Format.py -datasetName $DATASET \
+	-testFile 'data/conll2000_data/perturbed/03/test.txt' \
+	-testSetting 'CTP03_'$RUN'.txt' \
+	-cuda_device 0 \
+	-task 'pos'
+
+	echo "Test on p=0,4"
+	python EvalModel_CoNLL_Format.py -datasetName $DATASET \
+	-testFile 'data/conll2000_data/perturbed/04/test.txt' \
+	-testSetting 'CTP04_'$RUN'.txt' \
+	-cuda_device 0 \
+	-task 'pos'
+
+	echo "Test on p=0,5"
+	python EvalModel_CoNLL_Format.py -datasetName $DATASET \
+	-testFile 'data/conll2000_data/perturbed/05/test.txt' \
+	-testSetting 'CTP05_'$RUN'.txt' \
+	-cuda_device 0 \
+	-task 'pos'
+
+	echo "Test on p=0,6"
+	python EvalModel_CoNLL_Format.py -datasetName $DATASET \
+	-testFile 'data/conll2000_data/perturbed/06/test.txt' \
+	-testSetting 'CTP06_'$RUN'.txt' \
+	-cuda_device 0 \
+	-task 'pos'
+
+	echo "Test on p=0,7"
+	python EvalModel_CoNLL_Format.py -datasetName $DATASET \
+	-testFile 'data/conll2000_data/perturbed/07/test.txt' \
+	-testSetting 'CTP07_'$RUN'.txt' \
+	-cuda_device 0 \
+	-task 'pos'
+
+	echo "Test on p=0,8"
+	python EvalModel_CoNLL_Format.py -datasetName $DATASET \
+	-testFile 'data/conll2000_data/perturbed/08/test.txt' \
+	-testSetting 'CTP08_'$RUN'.txt' \
+	-cuda_device 0 \
+	-task 'pos'
+
+	echo "Test on p=0,9"
+	python EvalModel_CoNLL_Format.py -datasetName $DATASET \
+	-testFile 'data/conll2000_data/perturbed/09/test.txt' \
+	-testSetting 'CTP09_'$RUN'.txt' \
+	-cuda_device 0 \
+	-task 'pos'
+done 
+echo "Finished"
